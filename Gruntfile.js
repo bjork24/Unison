62098230c441fb834604b0411275e28e0ed79707
Dan Chilton <dan.chilton@gmail.com>
2013-12-14 17:05:11 -0600

    Changed name of responsive comments plugin

--- Gruntfile.js
+++ Gruntfile.js
@@ -55,8 +55,8 @@ module.exports = function (grunt) {
       my_target: {
         files: {
           'js/unison.min.js': ['js/unison.js'],
-          'js/unison-conditional-load.min.js': ['js/unison-conditional-load.js'],
-          'js/page.min.js': ['js/unison.js','js/unison-conditional-load.js','js/demo.js']
+          'js/unison-responsive-comments.min.js': ['js/unison-responsive-comments.js'],
+          'js/page.min.js': ['js/unison.js','js/unison-responsive-comments.js','js/demo.js']
         }
       }
     },
