aed5a376450a978534ef058f38cb3e660335b236
Dan Chilton <dan.chilton@gmail.com>
2013-12-29 15:13:20 -0600

    Make JS return null if breakpoints aren't properly set up

--- js/unison.js
+++ js/unison.js
@@ -4,13 +4,15 @@ Unison = (function() {
 
   // get breakpoint object
   var getBreakpoints = function(allBreakpoints) {
-    var allBP, currentBP, usnBP = {}, i = 0;
+    var allBP, currentBP, usnBP = {}, i = 0, doc = document, head = doc.head, win = window;
+    // check if css is passing breakpoints properly, if not return null for getBreakpoint()
+    var unisonReady = ( win.getComputedStyle(head, null).getPropertyValue('clear') === 'none' ) ? false : true ;
     function cleanMQstr(el) {
-      var str = window.getComputedStyle(el, null).getPropertyValue('font-family');
+      var str = win.getComputedStyle(el, null).getPropertyValue('font-family');
       return str.replace(/"/g, '').replace(/'/g, '');
     }
-    currentBP = cleanMQstr(document.head).split(' ');
-    allBP = cleanMQstr(document.querySelector('title')).split(',');
+    currentBP = cleanMQstr(head).split(' ');
+    allBP = cleanMQstr(doc.querySelector('title')).split(',');
     usnBP.currentBP = {
       name : currentBP[0],
       width : currentBP[1]
@@ -20,7 +22,11 @@ Unison = (function() {
       var mq = allBP[i].trim().split(' ');
       usnBP.allBP[mq[0]] = mq[1];
     }
-    return ( typeof allBreakpoints === 'boolean' ) ? usnBP.allBP : usnBP ;
+    if ( unisonReady ) {
+      return ( typeof allBreakpoints === 'boolean' ) ? usnBP.allBP : usnBP ;
+    } else {
+      return null;
+    }
   };
 
   // return some stuff because people like to see stuff in the console
